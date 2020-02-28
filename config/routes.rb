56e549b4bdc4762ab4b2e74f5b55441d78e43614
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-02-28 17:08:05 +0800

    added root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'cocktails/index'
-  get 'cocktails/show'
-  get 'cocktails/new'
+  root to: 'cocktails#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
